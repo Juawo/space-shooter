da825143c7728e9e690043cab33f635d81c2c17a
Juawo <joaopedrojp2703@gmail.com>
2026-01-24 16:59:28 -0300

    add menu_closed signal and improve main menu animation handling

--- scripts/ui/MainMenu/main_menu.gd
+++ scripts/ui/MainMenu/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal menu_closed
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 var showing: bool = true
 
@@ -10,3 +12,7 @@ func hideMainMenu():
 func showMainMenu():
 	animation_player.play_backwards("hide_menu")
 	showing = true
+
+func _on_animation_player_animation_finished(animationName: StringName) -> void :
+	if animationName == "hide_menu" and not showing :
+		menu_closed.emit()
