e1227169994c6750a1ea80fe1337bfde3b659b95
Juawo <joaopedrojp2703@gmail.com>
2026-01-26 13:20:25 -0300

    add animation_finished signal handling to main menu

--- scripts/ui/MainMenu/main_menu.gd
+++ scripts/ui/MainMenu/main_menu.gd
@@ -8,10 +8,14 @@ var showing: bool = true
 func hideMainMenu():
 	animation_player.play("hide_menu")
 	showing = false
+	await animation_player.animation_finished
+	self.visible = false
 	
 func showMainMenu():
 	animation_player.play_backwards("hide_menu")
 	showing = true
+	self.visible = true
+	
 
 func _on_animation_player_animation_finished(animationName: StringName) -> void :
 	if animationName == "hide_menu" and not showing :
