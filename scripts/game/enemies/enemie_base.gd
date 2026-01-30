d3de229b1bc237cd561a3d0fc9f4a8777828a656
Juawo <joaopedrojp2703@gmail.com>
2026-01-30 15:05:32 -0300

    Add enemy1 scene and behavior, including collision handling

--- scripts/game/enemies/enemie_base.gd
+++ scripts/game/enemies/enemie_base.gd
@@ -4,10 +4,14 @@ extends Area2D
 @export var score_value := 10
 @export var can_shoot := false
 
-func takeDamage():
-	life -= 1
+func takeDamage(damage_value: int) -> void:
+	life -= damage_value
 	if(life == 0):
 		die()
 
 func die():
 	queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	if( area.is_in_group("PlayerProjectiles")):
+		takeDamage(area.damage_value)
