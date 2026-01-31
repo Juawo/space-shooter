fad4fc0536054da39da7bccb267a2679658815d7
Juawo <joaopedrojp2703@gmail.com>
2026-01-31 16:31:25 -0300

    Refactor enemy base and enemy1 scenes: update collision shapes, enhance particle effects, and improve damage handling

--- scripts/game/enemies/enemie_base.gd
+++ scripts/game/enemies/enemie_base.gd
@@ -3,15 +3,21 @@ extends Area2D
 @export var life := 1
 @export var score_value := 10
 @export var can_shoot := false
+@export var base_color := Color.CHARTREUSE
+@onready var particle_die: CPUParticles2D = $ParticleDie
 
+func _ready() -> void:
+	particle_die.color = base_color
+	
 func takeDamage(damage_value: int) -> void:
 	life -= damage_value
 	if(life <= 0):
 		die()
 
 func die():
+	particle_die.emitting = true
 	queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
-	if( area.is_in_group("PlayerProjectiles")):
+	if(area.is_in_group("PlayerProjectiles")):
 		takeDamage(area.damage_value)
