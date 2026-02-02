b0945c92e769302e4b5f2fd6533a096cebbe3c4d
Juawo <joaopedrojp2703@gmail.com>
2026-02-01 22:45:42 -0300

    Refactor shoot function in Enemie4 to use a loop for bullet instantiation

--- scripts/game/enemies/enemie4/enemie_4.gd
+++ scripts/game/enemies/enemie4/enemie_4.gd
@@ -4,25 +4,18 @@ extends "res://scripts/game/enemies/enemie_base.gd"
 @onready var left_gun_point: Marker2D = $GunPoints/LeftGunPoint
 @onready var middle_gun_point: Marker2D = $GunPoints/MiddleGunPoint
 @onready var right_gun_point: Marker2D = $GunPoints/RightGunPoint
+@onready var gun_points: Node2D = $GunPoints
 
 func shoot():
 	if bullet_scene:
-		# Feio pq so queria que funcionasse
-		var bullet_left = bullet_scene.instantiate()
-		var bullet_middle = bullet_scene.instantiate()
-		var bullet_right = bullet_scene.instantiate()
-		
-		bullet_left.global_position = left_gun_point.global_position
-		bullet_middle.global_position = middle_gun_point.global_position
-		bullet_right.global_position = right_gun_point.global_position
-		
-		bullet_left.rotation = left_gun_point.rotation
-		bullet_middle.rotation = middle_gun_point.rotation
-		bullet_right.rotation = right_gun_point.rotation
-		
-		get_tree().current_scene.add_child(bullet_left)
-		get_tree().current_scene.add_child(bullet_middle)
-		get_tree().current_scene.add_child(bullet_right)
+		var bullets = []
+		for gun_point in gun_points.get_children():
+			var bullet = bullet_scene.instantiate()
+			bullet.global_position = gun_point.global_position
+			bullet.rotation = gun_point.rotation
+			bullets.append(bullet)
+		for bullet in bullets:
+			get_tree().current_scene.add_child(bullet)
 
 # Apenas para teste
 func _input(event: InputEvent) -> void:
