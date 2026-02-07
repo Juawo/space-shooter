e6bdbb185eabc42d4c772f8e915468f055aa6d41
Juawo <joaopedrojp2703@gmail.com>
2026-02-07 18:17:00 -0300

    Implement pause menu functionality and integrate with game state management

--- scripts/game/game-events.gd
+++ scripts/game/game-events.gd
@@ -2,3 +2,4 @@ extends Node
 
 signal pause_requested
 signal resume_requested
+signal main_menu_requested
--- scripts/game/main/main.gd
+++ scripts/game/main/main.gd
@@ -6,6 +6,7 @@ extends Node2D
 
 # "Componentes" do jogo
 @onready var main_menu : Control = $UI/MainMenu
+@onready var pause_menu: Control = $UI/PauseMenu
 
 # Enum com os estados possiveis do jogo
 enum GameStates { GAME, MAIN_MENU, PAUSED, GAME_OVER }
@@ -15,24 +16,43 @@ var state : GameStates = GameStates.MAIN_MENU : set = _set_state
 func _ready() -> void:
 	main_menu.connect("menu_closed",  _on_main_menu_closed)
 	GameEvents.pause_requested.connect(_on_pause_requested)
+	GameEvents.resume_requested.connect(_on_resume_requested)
+	GameEvents.main_menu_requested.connect(_on_main_menu_requested)
+	
+
+func _on_main_menu_requested():
+	state = GameStates.MAIN_MENU
 
 func _on_main_menu_closed():
 	state = GameStates.GAME
 
 func _on_pause_requested():
 	state = GameStates.PAUSED
-
+	
+func _on_resume_requested():
+	state = GameStates.GAME
+	
 func _set_state(newValue : GameStates):
 	state = newValue
 	match newValue  :
 		GameStates.MAIN_MENU :
+			if pause_menu.is_showing:
+				pause_menu.hide_pause_menu()
 			print("Game state changed to Main Menu")
 			main_menu.showMainMenu()
+			
 		GameStates.GAME :
-			main_menu.hideMainMenu()
+			get_tree().paused = false
+			if main_menu.showing:
+				main_menu.hideMainMenu()
+			if pause_menu.is_showing :
+				pause_menu.hide_pause_menu()
 			print("Game state changed to Game")
+			
 		GameStates.PAUSED :
 			get_tree().paused = true
+			pause_menu.show_pause_menu()
 			print("Game state changed to Paused")
+			
 		GameStates.GAME_OVER :
 			print("Game state changed to Game Over")
