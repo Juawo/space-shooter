79bce3536af60b13e46a2a5788ffebfb80c8ce91
Juawo <joaopedrojp2703@gmail.com>
2026-02-08 17:53:20 -0300

    Refactor main menu animations; add idle state playback and clean up unused animations

--- scripts/ui/MainMenu/main_menu.gd
+++ scripts/ui/MainMenu/main_menu.gd
@@ -13,9 +13,9 @@ func hideMainMenu():
 	
 func showMainMenu():
 	animation_player.play_backwards("hide_menu")
+	animation_player.play("idle")
 	showing = true
 	self.visible = true
-	
 
 func _on_animation_player_animation_finished(animationName: StringName) -> void :
 	if animationName == "hide_menu" and not showing :
