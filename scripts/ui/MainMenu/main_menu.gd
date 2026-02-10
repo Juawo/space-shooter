18909a159f9e5f6ee8697f029573899fb910a5c2
Juawo <joaopedrojp2703@gmail.com>
2026-02-10 11:38:18 -0300

    Refactor main menu scripts; improve tap area handling and animation flow

--- scripts/ui/MainMenu/main_menu.gd
+++ scripts/ui/MainMenu/main_menu.gd
@@ -2,18 +2,22 @@ extends Control
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 var showing: bool = false
+@onready var background_tap_area: Button = $BackgroundTapArea
 
 func hideMainMenu():
+	background_tap_area.disabled = true
 	animation_player.play("hide_menu")
 	showing = false
 	await animation_player.animation_finished
 	self.visible = false
 	
 func showMainMenu():
+	self.visible = true
 	animation_player.play_backwards("hide_menu")
+	await animation_player.animation_finished
 	animation_player.play("idle")
 	showing = true
-	self.visible = true
+	background_tap_area.disabled = false
 
 func _on_background_tap_area_pressed() -> void:
 	if is_visible_in_tree():
