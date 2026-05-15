881b4aa2f0152069cc9a6c0edd279d4ca5a472c2
Juawo <joaopedrojp2703@gmail.com>
2026-05-15 16:10:24 -0300

    fix: remove unused background scene and script to streamline game assets

--- scripts/game/background/background.gd
+++
@@ -1,7 +0,0 @@
-extends ParallaxBackground
-
-var scroll_speed = 120
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	scroll_offset.y += scroll_speed * delta
