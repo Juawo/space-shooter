f3681e617ffde51518a18e7adee9d730858ba35d
Juawo <joaopedrojp2703@gmail.com>
2026-06-04 01:52:45 -0300

    fix: correct export path in export presets and enhance registration feedback in UI

--- scripts/ui/Register/register.gd
+++ scripts/ui/Register/register.gd
@@ -24,13 +24,20 @@ signal register_pop_up_closed
 
 @onready var timer: Timer = $Timer
 
+@onready var register_again_label: Label = $MarginContainer/Panel/MarginContainer/VBoxContainer/register_again_label
+var register_again : bool = false 
+var is_country_valid : bool 
+		
+var is_nickname_valid : bool
+
 func _ready() -> void:
 	ApiManager.play_registered.connect(update_interface_by_response_code_return)
 
 func _on_button_pressed() -> void:
+	register_btn.disabled = true
 	if !validate_country(country_input.text) or !validate_nickname(nickname_input.text):
 		return
-		
+
 	var player_data = {
 		"Nickname" : nickname_input.text,
 		"Country" : country_input.text,
@@ -57,6 +64,7 @@ func update_interface_by_response_code_return(registered,response_code) -> void 
 			register_pop_up_closed.emit()
 			queue_free()
 		409 : 
+			print("DEU 54093")
 			detail_return.text = "Another player has this nickname."
 			detail_return.add_theme_color_override("font_color", "#d49a3d")
 		400 :
@@ -65,12 +73,29 @@ func update_interface_by_response_code_return(registered,response_code) -> void 
 		500 :
 			detail_return.text = "Error in the server side."
 			detail_return.add_theme_color_override("font_color", "#d4293d")
+		null :
+			detail_return.text = "You are offline. Try again when you are online."
+			detail_return.add_theme_color_override("font_color", "#d4293d")
+			timer.start(2)
+			await timer.timeout
+			animation_player.play("close_register")
+			await animation_player.animation_finished
+			register_pop_up_closed.emit()
+			queue_free()
 		_:
 			detail_return.text = "Type your info for register."
 			detail_return.add_theme_color_override("font_color", "#ffffff")
+	
 	nickname_input.editable = true
 	country_input.editable = true
 	return
+
+func show_register_again_label(display: bool) -> void :
+	register_again = display
+	if register_again :
+		register_again_label.visible = true
+		register_again_label.text = "You are not found in the database, please register again."
+		register_again_label.add_theme_color_override("font_color", "#d4293d")
 
 func update_register_btn(is_activated : bool) -> void :
 	register_btn.disabled = !is_activated 
@@ -80,14 +105,16 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 func _on_nickname_input_text_changed(new_text: String) -> void:
 	update_counter(new_text)
-	var is_nickname_valid = validate_nickname(new_text)
+	is_nickname_valid = validate_nickname(new_text)
 	update_nickname_feedback_validation_return(is_nickname_valid)
-	update_register_btn(is_nickname_valid)
+	if is_nickname_valid and is_country_valid:
+		update_register_btn(is_nickname_valid and is_country_valid)
 
 func _on_country_input_text_changed(new_text: String) -> void:
-	var is_country_valid = validate_country(new_text)
+	is_country_valid = validate_country(new_text)
 	update_country_feedback_validation_return(is_country_valid)
-	update_register_btn(is_country_valid)
+	if is_nickname_valid and is_country_valid:
+		update_register_btn(is_country_valid and is_nickname_valid)
 
 func update_counter(new_text : String) -> void :
 	char_count.text = "%d/12" % len(new_text)
