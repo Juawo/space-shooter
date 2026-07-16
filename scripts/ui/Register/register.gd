3fd909185ec7b5f30803f83a3c7045e9cfc5cfd9
Juawo <joaopedrojp2703@gmail.com>
2026-07-16 11:23:06 -0300

    feat: update registration form to include country selection and privacy policy acceptance

--- scripts/ui/Register/register.gd
+++ scripts/ui/Register/register.gd
@@ -8,12 +8,6 @@ signal register_pop_up_closed
 @onready var char_count: Label = $MarginContainer/Panel/MarginContainer/VBoxContainer/Nickname/nickname_type/char_count
 @onready var nickname_feedback: Label = $MarginContainer/Panel/MarginContainer/VBoxContainer/Nickname/nicknameFeedback
 
-@onready var country_input: LineEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/Country/CountryInput
-@onready var country_feedback: Label = $MarginContainer/Panel/MarginContainer/VBoxContainer/Country/CountryFeedback
-
-@onready var age_input: HSlider = $MarginContainer/Panel/MarginContainer/VBoxContainer/Age/AgeInput
-@onready var age_value: Label = $MarginContainer/Panel/MarginContainer/VBoxContainer/Age/AgeValue
-
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @onready var feedback: Label = $MarginContainer/Panel/MarginContainer/Feedback
@@ -23,31 +17,66 @@ signal register_pop_up_closed
 @onready var register_btn: Button = $MarginContainer/Panel/MarginContainer/VBoxContainer/register_btn
 
 @onready var timer: Timer = $Timer
+@onready var country_input: OptionButton = $MarginContainer/Panel/MarginContainer/VBoxContainer/Country/CountryInput
 
 @onready var register_again_label: Label = $MarginContainer/Panel/MarginContainer/VBoxContainer/register_again_label
 var register_again : bool = false 
 var is_country_valid : bool 
-		
+
 var is_nickname_valid : bool
+
+var accepted_policy : bool = false
+const COUNTRIES = {
+	"Brasil 🇧🇷": "BR",
+	"Estados Unidos 🇺🇸": "US",
+	"Portugal 🇵🇹": "PT",
+	"Angola 🇦🇴": "AO",
+	"Moçambique 🇲🇿": "MZ",
+	"Alemanha 🇩🇪": "DE",
+	"Argentina 🇦🇷": "AR",
+	"Austrália 🇦🇺": "AU",
+	"Canadá 🇨🇦": "CA",
+	"Chile 🇨🇱": "CL",
+	"China 🇨🇳": "CN",
+	"Colômbia 🇨🇴": "CO",
+	"Coreia do Sul 🇰🇷": "KR",
+	"Espanha 🇪🇸": "ES",
+	"França 🇫🇷": "FR",
+	"Índia 🇮🇳": "IN",
+	"Itália 🇮🇹": "IT",
+	"Japão 🇯🇵": "JP",
+	"México 🇲🇽": "MX",
+	"Reino Unido 🇬🇧": "GB",
+	"Rússia 🇷🇺": "RU",
+	"Uruguai 🇺🇾": "UY",
+	"Outro": "OT"
+}
 
 func _ready() -> void:
 	ApiManager.play_registered.connect(update_interface_by_response_code_return)
+	for i in COUNTRIES :
+		country_input.add_item(i)
 
 func _on_button_pressed() -> void:
 	SoundManager.play_click()
+	if !accepted_policy :
+		detail_return.text = "To register, you need to accept the privacy policy."
+		return
+	
 	register_btn.disabled = true
-	if !validate_country(country_input.text) or !validate_nickname(nickname_input.text):
+	if !validate_nickname(nickname_input.text):
 		return
 
 	var player_data = {
 		"Nickname" : nickname_input.text,
-		"Country" : country_input.text,
-		"Age" : int(age_input.value)
+		"Country" : get_selected_country(),
+		"Age" : 10
 	}
+	print(player_data)
 	ApiManager.register_player(player_data)
 	
 	nickname_input.editable = false
-	country_input.editable = false
+	country_input.disabled = true
 	
 	detail_return.text = "Awaiting server response..."
 	detail_return.add_theme_color_override("font_color", "#ffff")
@@ -65,7 +94,6 @@ func update_interface_by_response_code_return(registered,response_code) -> void 
 			register_pop_up_closed.emit()
 			queue_free()
 		409 : 
-			print("DEU 54093")
 			detail_return.text = "Another player has this nickname."
 			detail_return.add_theme_color_override("font_color", "#d49a3d")
 		400 :
@@ -88,7 +116,7 @@ func update_interface_by_response_code_return(registered,response_code) -> void 
 			detail_return.add_theme_color_override("font_color", "#ffffff")
 	
 	nickname_input.editable = true
-	country_input.editable = true
+	country_input.disabled = false
 	return
 
 func show_register_again_label(display: bool) -> void :
@@ -101,36 +129,14 @@ func show_register_again_label(display: bool) -> void :
 func update_register_btn(is_activated : bool) -> void :
 	register_btn.disabled = !is_activated 
 
-var old_value
-func _on_h_slider_value_changed(value: float) -> void:
-	age_value.text = str(int(value))
-	old_value = value
-	if value >= old_value :
-		SoundManager.play_scroll(true)
-	else :
-		SoundManager.play_scroll(false)
-		
 func _on_nickname_input_text_changed(new_text: String) -> void:
 	update_counter(new_text)
 	is_nickname_valid = validate_nickname(new_text)
 	update_nickname_feedback_validation_return(is_nickname_valid)
-	if is_nickname_valid and is_country_valid:
-		update_register_btn(is_nickname_valid and is_country_valid)
-
-func _on_country_input_text_changed(new_text: String) -> void:
-	is_country_valid = validate_country(new_text)
-	update_country_feedback_validation_return(is_country_valid)
-	if is_nickname_valid and is_country_valid:
-		update_register_btn(is_country_valid and is_nickname_valid)
+	update_register_btn(is_nickname_valid and accepted_policy)
 
 func update_counter(new_text : String) -> void :
 	char_count.text = "%d/12" % len(new_text)
-
-func validate_country(country_name : String) -> bool:
-	if len(country_name) <= 3 or country_name == " " or country_name.is_empty():
-		return false
-	else :
-		return true
 
 func validate_nickname(new_text : String) -> bool :
 	if len(new_text) <= 2 or new_text.count(" ") >= 1 or new_text.is_empty():
@@ -145,9 +151,16 @@ func update_nickname_feedback_validation_return(is_valid : bool) -> void :
 		nickname_feedback.text = "The nickname must be longer than 2 characters and contain no spaces."
 		nickname_feedback.add_theme_color_override("font_color", "#d4293d")
 
-func update_country_feedback_validation_return(is_valid : bool) -> void :
-	if is_valid :
-		country_feedback.text = ""
-	else :
-		country_feedback.text = "The country name must be longer than 3 characters"
-		country_feedback.add_theme_color_override("font_color", "#d4293d")
+func get_selected_country() -> String :
+	var selected_index = country_input.selected
+	var selected_text = country_input.get_item_text(selected_index)
+	
+	return COUNTRIES.get(selected_text, "OT") # "OT" como fallback (Outro)
+
+func _on_country_label_2_meta_clicked(meta: Variant) -> void:
+	OS.shell_open(str(meta))
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	accepted_policy = toggled_on
+	update_register_btn(is_nickname_valid and accepted_policy)
